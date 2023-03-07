b290ae982f8072b0acd683a77b0d59e1e2d2bcac
taku-0702 <motozawa-taku0702@outlook.jp>
2023-03-07 18:52:45 +0900

    recieve params from event

--- lambda_function.py
+++ lambda_function.py
@@ -1,12 +1,27 @@
-import os
+import os, sys
 import logging
-logging.basicConfig(level=logging.DEBUG)
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 from slack_sdk import WebClient
 client = WebClient(os.environ["SLACK_BOT_TOKEN"])
 
 def lambda_handler(event, context):
+  if "channel" in event:
+    channel = event['channel']
+  else:
+    logger.error("ERROR: specify slack channel.")
+    sys.exit()
+    
+  if "text" in event:
+    text = event["text"]
+  else:
+    logger.error("ERROR: specify text to post.")
+    sys.exit()
+  
   api_response = client.chat_postMessage(
-    channel="#practice",
-    text="こんにちは"
+    channel=channel,
+    text=text
   )
+  
